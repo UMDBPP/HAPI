08f6ab7f5d1aff3a05237485d446206c9089cced
Zach <zrb@umd.edu>
2018-10-08 19:13:20 -0400

    multiplexor example code

--- hapi.py
+++ hapi.py
@@ -1,4 +1,23 @@
+import os
+import sys
 
-message = 'hello world'
+sys.path.append(os.path.join(os.path.realpath(__file__), 'ivport-v2'))
 
-print(message)
+import ivport
+
+
+# raspistill capture
+def capture(camera):
+    "This system command for raspistill capture"
+    cmd = "raspistill -t 10 -o still_CAM%d.jpg" % camera
+    os.system(cmd)
+
+
+with ivport.IVPort(ivport.TYPE_DUAL2) as multiplexor:
+    multiplexor.camera_change(1)
+
+    capture(1)
+    multiplexor.camera_change(2)
+    
+    capture(2)
+    multiplexor.close()
