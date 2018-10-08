41114ba0fe67d342ff9c3332643126008509d36b
Zach <zrb@umd.edu>
2018-10-08 19:24:08 -0400

    example commit

--- hapi.py
+++ hapi.py
@@ -6,18 +6,14 @@ sys.path.append(os.path.join(os.path.realpath(__file__), 'ivport-v2'))
 import ivport
 
 
-# raspistill capture
 def capture(camera):
-    "This system command for raspistill capture"
-    cmd = "raspistill -t 10 -o still_CAM%d.jpg" % camera
+    cmd = f'raspistill -t 10 -o still_CAM{camera}.jpg'
     os.system(cmd)
 
 
 with ivport.IVPort(ivport.TYPE_DUAL2) as multiplexor:
     multiplexor.camera_change(1)
-
     capture(1)
+
     multiplexor.camera_change(2)
-    
     capture(2)
-    multiplexor.close()
