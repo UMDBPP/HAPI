9ca19e569392360fb4cb8558c4632bb5d3eab059
Zach <zrb@umd.edu>
2018-10-27 13:11:45 -0400

    added submodule and picamera code

--- hapi.py
+++ hapi.py
@@ -1,19 +1,30 @@
 import os
 import sys
 
-sys.path.append(os.path.join(os.path.realpath(__file__), 'ivport-v2'))
+# sys.path.append(os.path.join(os.path.realpath(__file__), 'ivport'))
 
 import ivport
+import ivport.picamera
+import time
 
+# def capture(camera):
+#     cmd = f'raspistill -t 10 -o still_CAM{camera}.jpg'
+#     os.system(cmd)
+#
+#
+# with ivport.IVPort(ivport.TYPE_QUAD2) as multiplexor:
+#     multiplexor.camera_change(1)
+#     capture(1)
+#
+#     multiplexor.camera_change(2)
+#     capture(2)
 
-def capture(camera):
-    cmd = f'raspistill -t 10 -o still_CAM{camera}.jpg'
-    os.system(cmd)
+with ivport.picamera.PiCamera() as camera:
+    camera.resolution = (640, 480)
+    camera.framerate = 90
+    camera.start_preview()
 
-
-with ivport.IVPort(ivport.TYPE_DUAL2) as multiplexor:
-    multiplexor.camera_change(1)
-    capture(1)
-
-    multiplexor.camera_change(2)
-    capture(2)
+    time.sleep(2)    # Camera Initialize
+    start = time.time()
+    camera.capture_sequence([], use_video_port=True)
+    finish = time.time()
