da3553a46e8b8a43a04924b60f2a0a1dd3023dea
Zach <zrb@umd.edu>
2018-10-27 15:24:24 -0400

    operational code for thermal test

--- hapi.py
+++ hapi.py
@@ -1,19 +1,34 @@
 import os
 import sys
+import time
 
 sys.path.append(os.path.join(os.path.realpath(__file__), 'ivport'))
 
 import ivport
 
+timeout_seconds = 30
+picamera_capture_interval = 5
 
 def capture(camera):
-    cmd = f'raspistill -t 10 -o still_CAM' + camera + '.jpg'
+    cmd = 'raspistill -t 10 -o /home/pi/Desktop/hapi_images/still_CAM' + camera + '.jpg'
     os.system(cmd)
 
+image_index = 0
 
-with ivport.IVPort(ivport.TYPE_QUAD2) as multiplexor:
-    multiplexor.camera_change(1)
-    capture(1)
+logging_start_time = time.time()
 
-    multiplexor.camera_change(2)
-    capture(2)
+with ivport.IVPort(ivport.TYPE_QUAD2) as multiplexer:
+    while time.time() < logging_start_time + timeout_seconds:
+        multiplexer.camera_change(1)
+        capture(1)
+
+        multiplexer.camera_change(2)
+        capture(2)
+
+        multiplexer.camera_change(3)
+        capture(3)
+
+        multiplexer.camera_change(4)
+        capture(4)
+
+        time.sleep(picamera_capture_interval)
